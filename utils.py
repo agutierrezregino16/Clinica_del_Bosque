8c654ddbcffa44afa4bb5f7907e87e149bc8d456
ogonzalez03 <orlandogomo@gmail.com>
2021-10-13 22:17:31 -0500

    Correccion de errores menores templates

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import re
 from validate_email import validate_email
 
-pass_reguex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$"
-user_reguex = "^[a-zA-Z0-9_.-]+$"
+pass_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$"
+user_regex = "^[a-zA-Z0-9_.-]+$"
 F_ACTIVE = 'ACTIVE'
 F_INACTIVE = 'INACTIVE'
 EMAIL_APP = 'EMAIL_APP'
@@ -16,14 +16,9 @@ def isEmailValid(email):
     is_valid = validate_email(email)
     return is_valid
 
-def isUsernameValid(user):
-    if re.search(user_reguex, user):
-        return True
-    else:
-        return False
 
 def isPasswordValid(password):
-    if re.search(pass_reguex, password):
+    if re.search(pass_regex, password):
         return True
     else:
         return False
